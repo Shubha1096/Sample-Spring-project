596b6b485682a0e69fc6ccfe6b640f1dfb105569
Shubham Khandelwal <Khandelwalshubham1996@gmail.com>
2025-01-21 11:01:18 +0530

    First Commit

---
+++ src/main/java/com/bezkoder/spring/jpa/postgresql/controller/TutorialController.java
@@ -0,0 +1,54 @@
+package com.bezkoder.spring.jpa.postgresql.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bezkoder.spring.jpa.postgresql.model.Tutorial;
+import com.bezkoder.spring.jpa.postgresql.repository.TutorialRepository;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+public class TutorialController {
+
+	@Autowired
+	TutorialRepository tutorialRepository;
+
+	@GetMapping("/tutorials")
+	public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+		try {
+		
+			List<Tutorial> tutorials = new ArrayList<Tutorial>();
+
+			if (title == null) {
+				tutorials = tutorialRepository.findAll();
+			} else {
+				tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+			}
+
+			if (tutorials.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+
+			return new ResponseEntity<>(tutorials, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
